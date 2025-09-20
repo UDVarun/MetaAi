7122b9162c996598c463b7450eb747fd4cf88d43
Varun UD <varunud84@gmail.com>
2025-09-20 20:16:56 +0530

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="text-2xl text-amber-200 font-bold">
+    hello world
+    <Button>Click me</Button>
+    </div>
+  );
+}
