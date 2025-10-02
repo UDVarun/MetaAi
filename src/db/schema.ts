ef2a93b615cb34b459c71ed18bb08113327b2917
Varun UD <varunud84@gmail.com>
2025-10-02 20:10:08 +0530

    09: agents setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,9 @@
+import { de, id } from "date-fns/locale";
 import {pgTable, text, timestamp, boolean} from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
+
+// better-auth schema
 export const user = pgTable("user", {
     id: text('id').primaryKey(),
     name: text('name').notNull(),
@@ -45,3 +49,13 @@ export const verification = pgTable("verification", {
     createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
     updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
 });
+
+    // meetai schema
+export const agents=pgTable("agents",{
+    id: text('id').primaryKey().$defaultFn(() => nanoid()),
+    name: text('name').notNull(),
+    userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
+    instructions: text('instructions').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+}); 
