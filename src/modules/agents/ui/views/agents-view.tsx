9dc7d2ae3d6365341fb8750c83a922add3370a2e
Varun UD <varunud84@gmail.com>
2025-10-22 12:25:29 +0530

    10 : Responsive Dailog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,28 +5,29 @@ import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 
+
 export const AgentsView = () => {
     const trpc = useTRPC();
     const { data, isLoading, isError } = useQuery(trpc.agents.getMany.queryOptions());
 
-    if(isLoading) {
-        return ( 
-        <LoadingState 
-        title="Loading agents"
-        description="Please wait while we load the agents"
-        />
-    )};
+    // if(isLoading) {
+    //     return ( 
+    //     <LoadingState 
+    //     title="Loading agents"
+    //     description="Please wait while we load the agents"
+    //     />
+    // )};
 
-    if(isError){
-        return ( 
-        <ErrorState 
-        title="Error loading agents"
-        description="There was an error loading the agents"
-        />
+    // if(isError){
+    //     return ( 
+    //     <ErrorState 
+    //     title="Error loading agents"
+    //     description="There was an error loading the agents"
+    //     />
 
-    )};
+    // )};
     return (
-        <div>
+        <div> 
             {JSON.stringify(data, null, 2)}
         </div>
     )
